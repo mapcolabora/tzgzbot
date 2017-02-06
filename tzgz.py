0cc45bc56faccaa393f1c92640dc98ef15f6e706
Robot8A <cie.hochoa@gmail.com>
2017-02-06 15:24:18 +0100

    Añadir imágenes en /bizi

--- tzgz.py
+++ tzgz.py
@@ -243,12 +243,20 @@ def bizi(bot, update, args):
         except Exception as e:
             bot.sendMessage(chat_id=update.message.chat_id, text='‼️<b>Error</b>‼️\nImposible contactar con el servicio del Ayuntamiento.', parse_mode='HTML')
         jsonleido = json.loads(str(f.read().decode('utf-8')))
+        foto = ''
+        try:
+            g = urllib.request.urlopen('http://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea%283600345740%29%2D%3E%2EsearchArea%3B%28node%5B%22amenity%22%3D%22bicycle%5Frental%22%5D%5B%22network%22%3D%22BiZi%22%5D%5B%22ref%22%3D%22'+numposte+'%22%5D%28area%2EsearchArea%29%3B%29%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B%0A')#Overpass request para la estación BiZi
+            overpassleido = json.loads(str(g.read().decode('utf-8')))
+            g.close()
+            foto = '\n<a href="https://www.mapillary.com/app/?pKey='+overpassleido["elements"][0]["tags"]["mapillary"]+'&focus=photo">📷 Foto</a>'
+        except:
+            print('No se ha encontrado en Overpass API: /bizi '+numposte)
         estado=jsonleido["estado"]
         if estado=='OPN':
             estado=' ✅'
         elif estado=='':#TODO averiguar qué estado se pone cuando una estación no está operativa
             estado=' ⚠️'
-        bot.sendMessage(chat_id=update.message.chat_id, text='Estación número '+jsonleido["id"]+estado+'\n'+jsonleido["title"]+'\n<a href="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0Aarea(3600345740)-%3E.searchArea%3B%0A(%0A%20%20node%5B%22amenity%22%3D%22bicycle_rental%22%5D%5B%22network%22%3D%22BiZi%22%5D%5B%22ref%22%3D%22'+numposte+'%22%5D(area.searchArea)%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B%0A%0A%0A%0A%7B%7Bstyle%3A%20%0A%20%20node%20%7B%20color%3Ared%3B%20fill-color%3Ared%3B%20fill-opacity%3A1%3B%20text%3A%20name%3B%20%20%7D%0A%20%7D%7D">🗺 Mapa</a>\n\n🚲Bicis disponibles: '+str(jsonleido["bicisDisponibles"])+'\n🚴Anclajes disponibles: '+str(jsonleido["anclajesDisponibles"]), parse_mode='HTML', disable_web_page_preview=True)#el link del mapa hace esta query overpass (cambiar 66 por el número de poste deseado): http://overpass-turbo.eu/s/lhL
+        bot.sendMessage(chat_id=update.message.chat_id, text='Estación número '+jsonleido["id"]+estado+'\n'+jsonleido["title"]+'\n<a href="http://overpass-turbo.eu/map.html?Q=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0Aarea(3600345740)-%3E.searchArea%3B%0A(%0A%20%20node%5B%22amenity%22%3D%22bicycle_rental%22%5D%5B%22network%22%3D%22BiZi%22%5D%5B%22ref%22%3D%22'+numposte+'%22%5D(area.searchArea)%3B%0A)%3B%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B%0A%0A%0A%0A%7B%7Bstyle%3A%20%0A%20%20node%20%7B%20color%3Ared%3B%20fill-color%3Ared%3B%20fill-opacity%3A1%3B%20text%3A%20name%3B%20%20%7D%0A%20%7D%7D">🗺 Mapa</a>'+foto+'\n\n🚲Bicis disponibles: '+str(jsonleido["bicisDisponibles"])+'\n🚴Anclajes disponibles: '+str(jsonleido["anclajesDisponibles"]), parse_mode='HTML', disable_web_page_preview=True)#el link del mapa hace esta query overpass (cambiar 66 por el número de poste deseado): http://overpass-turbo.eu/s/lhL
         if estado==' ⚠️':
             paradasBiziCercanas(bot,update,str(jsonleido["geometry"]["coordinates"][0]),str(jsonleido["geometry"]["coordinates"][1]),'La estación BiZi '+jsonleido["id"]+' no está operativa',numposte)
         elif jsonleido["bicisDisponibles"]==0:
@@ -274,7 +282,7 @@ def paradasBiziCercanas(bot,update,longitud,latitud,estado,numposte):
         for i in range(nElementosbizi):
             if(jsonleidobizi["result"][i]["id"]!=numposte):#no enseñar la parada desde la que se invoca
                 textobizi = textobizi + '/bizi '+ jsonleidobizi["result"][i]["id"] + '\n' + jsonleidobizi["result"][i]["title"] + '\n\n'
-    bot.sendMessage(chat_id=update.message.chat_id, text='<b>'+estado+', quizás te interesen otras estaciones cercanas:</b>\n'+textobizi, parse_mode='HTML', disable_web_page_preview=True)
+    bot.sendMessage(chat_id=update.message.chat_id, text='<b>'+estado+', quizás te interesen otras estaciones cercanas (a &lt'+DISTANCIA+'m):</b>\n'+textobizi, parse_mode='HTML', disable_web_page_preview=True)
         
 def help(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text='Bot sobre el transporte público en Zaragoza, "powered by" contribuidores de <a href="https://www.openstreetmap.org/">OpenStreetMap</a> y <a href="http://www.zaragoza.es/ciudad/risp/api.htm">datos abiertos del Ayuntamiento de Zaragoza</a>. Programado por @Robot8A.\n\n<a href="https://github.com/mapcolabora/tzgzbot">Código fuente</a>, licencia GPLv3+\n\nImagen de perfil por @Robot8A, <a href="https://commons.wikimedia.org/wiki/File:Parada_bus_Balc%C3%B3n_San_L%C3%A1zaro.jpg">ver original</a>, CC-BY.\n\nAgradecimientos a <a href="http://pulsar.unizar.es/">Púlsar</a> por el hosting del bot.\n\n\n<b>FUNCIONES DEL BOT:</b>\n<b>Mandar ubicación</b> - Te devuelve un listado de las paradas a 200 metros de la ubicación\n<b>/bus &lt;númerodeposte&gt;</b> - Tiempo real de los postes de bus\n<b>/linbus &lt;númerodelínea&gt;</b> - Plano de la línea de bus correspondiente\n<b>/tram &lt;númerodeposte&gt;</b> - Tiempo real de los postes del tranvía\n<b>/lintram</b> - Plano de la línea de tranvía\n<b>/bizi &lt;númerodeestación&gt;</b> - Estado en tiempo real de las estaciones de BiZi\n<b>/mapatransporte</b> - Mapa con todos los medios de transporte\n<b>/mapabici</b> - Mapa de la infraestructura ciclista en Zaragoza\n<b>/mapataxi</b> - Mapa de las paradas de Taxi y taxis en tiempo real\n<b>/ruta</b> - Usar el calculador de rutas del Ayuntamiento\n<b>/faq</b> - Muestra la respuesta a las preguntas más frecuentes sobre el bot\n<b>/help</b> - Muestra este mensaje', parse_mode='HTML', disable_web_page_preview=True)
